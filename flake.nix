491f08af501d1dff4ff09d50b0757fc71db4da6b
Kyan Van den Eynde <kyanvde@gmail.com>
2025-09-17 20:25:55 +0200

    feat(root): ✨ add home-manager

--- flake.nix
+++ flake.nix
@@ -4,16 +4,26 @@
   
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-25.05";
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs"; # Ensures compatibility between nixpkgs and home-manager versions.
   };
 
-  outputs = { self, nixpkgs, ... }:
+  outputs = { self, nixpkgs, home-manager, ... }:
     let
         lib = nixpkgs.lib;
+        system = "x86_64-linux";
+        pkgs = nixpkgs.legacyPackages.${system};
     in {
     nixosConfigurations = {
       nixos = lib.nixosSystem {
-        system = "x86_64-linux";
+        inherit system;
         modules = [ ./configuration.nix ];
+      };
+    };
+    homeConfigurations = {
+      kyan = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [ ./home.nix ];
       };
     };
   };
